830435106bb0455984f5f4640929c2a83acb83b0
BitofaPhilistine <philipgowell@gmail.com>
2026-05-22 21:10:59 -0400

    Added framework for sub-cells, option to save to output.txt, and improved printing display to be more dynamic

--- main.py
+++ main.py
@@ -7,22 +7,25 @@ class Char:
         self.char = char
         self.brightness = brightness
         self.pixels = pixels
+        self.subCells = [[0 for y in range(12)] for x in range(6)]
     
     def __repr__(self):
-        return f"{self.char}{self.brightness / (self.pixels.size[0] * self.pixels.size[1])}"
+        # Return a string with the character, its average brightness, and the average brightness of each subcell
+        return f"{self.char}{self.brightness / (self.pixels.size[0] * self.pixels.size[1])},{','.join([str(self.subCells[x][y] / (self.pixels.size[0] * self.pixels.size[1] / 72)) for x in range(6) for y in range(12)])}"
 
 
 class Cell:
     def __init__(self, brightness: int = 0, pixels: Image.Image = Image.new("1", (1, 1))):
         self.brightness = brightness
         self.pixels = pixels
+        self.subCells = [[0 for y in range(12)] for x in range(6)]
 
 
 def roundToMultiple(value: int|float, multiple: int|float) -> int|float:
     return round(value / multiple) * multiple
 
 
-def printProgressBar(progress: float, length: int = 20):
+def printProgressBar(progress: float, length: int):
     if progress < 1.0:
         print("\r[" + "■" * math.floor(progress * length) + "-" * (length - math.floor(progress * length)) + "]", end="")
     else:
@@ -36,14 +39,14 @@ def main():
 
     # Get image name from user and confirm it exists in the Images folder
     print("Enter the name of the image you want to convert to ASCII art (must be in the Images folder):")
-    imagePath = f"images/{input('')}"
+    imagePath = f"images/{input(':')}"
     while not os.path.exists(imagePath):
         print("Image not found. Name must include file extension (e.g. .jpg, .png) and image must be in the Images folder.")
-        imagePath = f"images/{input('')}"
+        imagePath = f"images/{input(':')}"
 
     # Get font size
     print("Enter the font size, in pixels, higher values result in a smaller and less detailed output: ")
-    fontSize = input('')
+    fontSize = input(':')
     try:
         fontSize = int(fontSize)
     except ValueError:
@@ -51,7 +54,7 @@ def main():
 
     while fontSize <= 0:
         print("Font size must be a positive integer.")
-        fontSize = int(input(''))
+        fontSize = int(input(':'))
         try:
             fontSize = int(fontSize)
         except ValueError:
@@ -61,7 +64,7 @@ def main():
 
     # Get the image color complexity for the edge detection filter, higher values result in a more detailed image, but can also result in more noise
     print("Enter the image color complexity for the edge detection filter (higher values result in more detail but can also result in more noise, 3 recommended):")
-    colorComplexity = input('')
+    colorComplexity = input(':')
     try:
         colorComplexity = int(colorComplexity)
     except ValueError:
@@ -69,7 +72,7 @@ def main():
     
     while colorComplexity <= 0:
         print("Color complexity must be a positive integer.")
-        colorComplexity = int(input(''))
+        colorComplexity = int(input(':'))
         try:
             colorComplexity = int(colorComplexity)
         except ValueError:
@@ -93,12 +96,19 @@ abcdefghijklmnopqrstuvwxyz{|}~∙·
     if not os.path.exists(fontDataPath):
         print("Font data file not found, creating font data file...")
         # initialize the chars list with an image for each character and calculate the brightness of each character
+        tempFont = ImageFont.truetype("fonts/CascadiaMono-Regular.ttf", 20)
         for char in ascii:
-            newChar = Char(char, 0, Image.new("1", cellSize))
-            ImageDraw.Draw(newChar.pixels).text((0, 0), char, font=font, fill=255)
-            for pixel in newChar.pixels.get_flattened_data():
-                newChar.brightness += 1 if pixel > 0 else 0
+            printProgressBar((len(chars)) / len(ascii), 50)
+            newChar = Char(char, 0, Image.new("1", (12, 24)))
+            ImageDraw.Draw(newChar.pixels).text((0, 0), char, font=tempFont, fill=255)
+            for x in range(newChar.pixels.size[0]):
+                for y in range(newChar.pixels.size[1]):
+                    pixel = newChar.pixels.getpixel((x, y))
+                    if pixel > 0:
+                        newChar.brightness += 1
+                        newChar.subCells[x // round(newChar.pixels.size[0] / 6)][y // round(newChar.pixels.size[1] / 12)] += 1
             chars.append(newChar)
+            printProgressBar((len(chars)) / len(ascii), 50)
         
         # Save the font data to a file
         with open(fontDataPath, 'w') as f:
@@ -106,15 +116,20 @@ abcdefghijklmnopqrstuvwxyz{|}~∙·
             for char in chars:
                 data += f"{char}\n"
             f.write(data)
+            print("Font data saved.")
     else:
         print("Font data file found, loading font data...")
-        with open(fontDataPath, 'r') as f:
-            for line in f:
-                char = line[0]
-                newChar = Char(char, 0, Image.new("1", cellSize))
-                ImageDraw.Draw(newChar.pixels).text((0, 0), char, font=font, fill=255)
-                newChar.brightness = round(float(line[1:]) * (cellSize[0] * cellSize[1]))
-                chars.append(newChar)
+    with open(fontDataPath, 'r') as f:
+        for line in f:
+            char = line[0]
+            newChar = Char(char, 0, Image.new("1", cellSize))
+            ImageDraw.Draw(newChar.pixels).text((0, 0), char, font=font, fill=255)
+            floats = line[1:].split(',')
+            newChar.brightness = round(float(floats[0]) * (cellSize[0] * cellSize[1]))
+            for x in range(6):
+                for y in range(12):
+                    newChar.subCells[x][y] = round(float(floats[1 + x * 12 + y]) * (cellSize[0] * cellSize[1] / 72))
+            chars.append(newChar)
     
     # Check if the target image resolution is a multiple of the cell size, if not round it to the nearest multiple
     image = Image.open(imagePath)
@@ -178,7 +193,6 @@ abcdefghijklmnopqrstuvwxyz{|}~∙·
     output = []
     for y in range(len(imageCells[0])):
         output.append("░" * len(imageCells))
-    print(len(output))
     
     for y in range(len(imageCells[0])):
         for x in range(len(imageCells)):
@@ -196,9 +210,20 @@ abcdefghijklmnopqrstuvwxyz{|}~∙·
                     elif i == len(cell.pixels.get_flattened_data()) - 1:
                         closestDiff = charDiff
                         closestChar = char.char
+                print("\r" + output[y][:x] + closestChar + output[y][x + 1:], end="")
             output[y] = output[y][:x] + closestChar + output[y][x + 1:]
             print("\r" + output[y], end="")
         print("\r" + output[y])
+    
+    print("Conversion complete. Save output to a text file? (y/n)")
+    if input(':').lower() == 'y':
+        with open("output.txt", 'w') as f:
+            f.write("\n".join(output))
+        print("Output saved to output.txt.")
+        sys.exit(0)
+    else:
+        print("Output not saved.")
+        sys.exit(0)
 
 
 if __name__ == "__main__":
